74da8d42d2055d55ac5e7ae58baf906cdf55cded
Your Name <you@example.com>
2017-09-19 17:07:38 +0300

    create product migration

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, 'sqlite3:pizza.db'
+
+class Product < ActiveRecord::Base
+end
+
 
 get '/' do
 	erb :index
